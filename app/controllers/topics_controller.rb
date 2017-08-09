7bd10798db966bb95ea2b01fd905fdd54d825ac5
junichiiwakawa <df533738@db3.so-net.ne.jp>
2017-08-09 04:09:11 +0000

    facebook

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,49 @@
+class TopicsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+
+
+  def index
+    @topics = Topic.all
+  end
+  
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    #Topic.create(topics_params)
+    @topic = Topic.new(topics_params) #newメソッドで、空のインスタンスを作成し、値を代入した上で保存する。
+    if @topic.save
+     redirect_to topics_path, notice: "トピックを作成しました！"
+    else
+     render 'new'
+    end
+  end
+  
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+  
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.update(topics_params)
+    redirect_to topics_path, notice: "トピックを更新しました！"
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_path, notice: "トピックを削除しました！"
+  end
+  
+  private
+    def topics_params
+      params.require(:topic).permit(:title, :content)
+    end
+    
+  # idをキーとして値を取得するメソッド
+    def set_blog
+      @topic = Topic.find(params[:id])
+    end  
+    
+end
