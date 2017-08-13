a582e28c5657f9cde33addb5f6b6565e26a8fb49
iwakawa <df533738@db3.so-net.ne.jp>
2017-08-13 14:40:56 +0900

    facebook

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'https://ancient-forest-56348.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: '13.113.112.85' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
